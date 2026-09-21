3ec548a800f45840bc86f0e5876b9b44fbf966c9
montaeg0920-lang <montaeg0920@gmail.com>
2026-09-21 23:04:17 +0900

    Fix TypeScript entry import

--- tituswebdesign/src/main.tsx
+++ tituswebdesign/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import './index.css' // Note: Ensure you have @tailwind directives in a .css file
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
